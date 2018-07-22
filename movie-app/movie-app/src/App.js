512253187a1ee07c242015a236c6de527d05db3e
jae <kjlee.ko@gmail.com>
2018-07-22 17:02:13 +0900

    progressed until lesson 16

--- movie-app/movie-app/src/App.js
+++ movie-app/movie-app/src/App.js
@@ -3,28 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-  /** 
-   * React Component LifeCycle (Deprecated 된 것들이 많으므로 실제 사용시 확인 필요)
-   * 
-   * Create
-   * 
-   *    constructor(): 컴포넌트가 생성되었을때 호출
-   *    -> componentWillMount(): render() 전에 호출
-   *    -> render(): view를 만들어주는 함수
-   *    -> componentDidMount(): render() 후에 호출
-   * 
-   * Update
-   * 
-   *    componentWillReceiveProps(): Component가 새로운 Props를 받게 됐을때 호출
-   *    -> shouldComponentUpdate(): Component의 Props나 State 변경 시 호출; default는 true이며 조건에 따라 render()를 호출
-   *    -> componentWilUpdate(): shouldComponentUpdate에서 true를 return할 시 호출
-   *    -> render() 
-   *    -> componentDidUpdate(): Component가 업데이트 된 후 호출되며 preProps와 preState 조회가 가능
-   * 
-   * Destroy
-   * 
-   *    componentWillUnmount(): Component가 Destroy된 후 호출된다.
-   */
+//import axios from 'axios';
+
+//https://yts.am/api/v2/list_movies.json?sort_by=rating
+
+
+/** 
+ * React Component LifeCycle (Deprecated 된 것들이 많으므로 실제 사용시 확인 필요)
+ * 
+ * Create
+ * 
+ *    constructor(): 컴포넌트가 생성되었을때 호출
+ *    -> componentWillMount(): render() 전에 호출
+ *    -> render(): view를 만들어주는 함수
+ *    -> componentDidMount(): render() 후에 호출
+ * 
+ * Update
+ * 
+ *    componentWillReceiveProps(): Component가 새로운 Props를 받게 됐을때 호출
+ *    -> shouldComponentUpdate(): Component의 Props나 State 변경 시 호출; default는 true이며 조건에 따라 render()를 호출
+ *    -> componentWilUpdate(): shouldComponentUpdate에서 true를 return할 시 호출
+ *    -> render() 
+ *    -> componentDidUpdate(): Component가 업데이트 된 후 호출되며 preProps와 preState 조회가 가능
+ * 
+ * Destroy
+ * 
+ *    componentWillUnmount(): Component가 Destroy된 후 호출된다.
+ */
+
+const MOVIE_LIST = "https://yts.am/api/v2/list_movies.json?sort_by=like_count";
 
 class App extends Component {
 
@@ -33,42 +40,35 @@ class App extends Component {
   state = {
   }
 
+  _callApi = () => {
+    // return Promise<Response> by AJAX
+    return fetch(MOVIE_LIST)
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+
+    // using axios library
+    // axios.get(MOVIE_LIST)
+    // .then(response => response.data)
+    // .then(response => console.log(response))
+    // .catch(err => console.log(err))
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
   componentDidMount() {
-    console.log("componentDidMount")
-    setTimeout( () => {
-      // state는 setState()를 이용해서 update해야 LifeCycle의 메서드들이 정상 호출 된다.
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://imgc.allpostersimages.com/img/posters/the-matrix_u-L-F4S5W20.jpg?src=gp&w=300&h=375"
-          },
-          {
-            title: "Mission Impossible",
-            poster: "https://vignette.wikia.nocookie.net/missionimpossible/images/c/cf/Mission_Impossible_Fallout_poster.jpg/revision/latest/scale-to-width-down/323?cb=20180202011058"
-          },
-          {
-            title: "Star Wars",
-            poster: "http://www.impawards.com/2017/posters/star_wars_the_last_jedi_ver58.jpg"
-          },
-          {
-            title: "007",
-            poster: "https://66.media.tumblr.com/72eea0fd18af175c7e5ac365156f2220/tumblr_ourocddaad1qc2maxo1_500.png"
-          },
-          {
-            title: "Dark Knight",
-            poster: "https://i.ebayimg.com/images/g/BYwAAOSwmfhX5n5v/s-l300.jpg"
-          }
-        ]
-      })
-    }, 3000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map( (movie, index) => {
-      console.log(index);
+    const movies = this.state.movies.map( movie => {
       // 엘리먼트가 많은 경우 key를 넣어 줘야 함
-      return < Movie title={movie.title} poster={movie.poster} key={index}/>
+      return < Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
