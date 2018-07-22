f5356d21ddd187457475a8360a6da061d442528f
jae <kjlee.ko@gmail.com>
2018-07-22 14:33:43 +0900

    progressed until lesson 11

--- movie-app/movie-app/src/Movie.js
+++ movie-app/movie-app/src/Movie.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 class Movie extends Component {
+  
+  /**
+   * static propTypes = { ... } 를 override 해서 props의 type을 명시 할 수 있다.
+   * isRequired: 필수 parameter를 정의할 수 있다.
+   */ 
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
+  
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
         < MoviePoster poster={this.props.poster}/>
-      </div>
+      </div> 
     );
   }
 }
 
 class MoviePoster extends Component {
+
+  static propTypes = {
+    poster: PropTypes.string.isRequired,
+  }
+
   render() {
     return (
       <img src={this.props.poster} alt="Movie Poster" />
