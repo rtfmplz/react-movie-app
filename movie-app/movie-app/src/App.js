86260d724be968f1a2e4b95a83f5dc3835010c47
jae <kjlee.ko@gmail.com>
2018-07-22 15:08:20 +0900

    progressed until lesson 12

--- movie-app/movie-app/src/App.js
+++ movie-app/movie-app/src/App.js
@@ -31,24 +31,6 @@ class App extends Component {
   // state는 React Component 안의 object
   // state가 바뀔 때 마다, Update 가 발생한다.
   state = {
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://imgc.allpostersimages.com/img/posters/the-matrix_u-L-F4S5W20.jpg?src=gp&w=300&h=375"
-      },
-      {
-        title: "Mission Impossible",
-        poster: "https://vignette.wikia.nocookie.net/missionimpossible/images/c/cf/Mission_Impossible_Fallout_poster.jpg/revision/latest/scale-to-width-down/323?cb=20180202011058"
-      },
-      {
-        title: "Star Wars",
-        poster: "http://www.impawards.com/2017/posters/star_wars_the_last_jedi_ver58.jpg"
-      },
-      {
-        title: "007",
-        poster: "https://66.media.tumblr.com/72eea0fd18af175c7e5ac365156f2220/tumblr_ourocddaad1qc2maxo1_500.png"
-      }
-    ]
   }
 
   componentDidMount() {
@@ -57,25 +39,44 @@ class App extends Component {
       // state는 setState()를 이용해서 update해야 LifeCycle의 메서드들이 정상 호출 된다.
       this.setState({
         movies: [
-          // append list
+          {
+            title: "Matrix",
+            poster: "https://imgc.allpostersimages.com/img/posters/the-matrix_u-L-F4S5W20.jpg?src=gp&w=300&h=375"
+          },
+          {
+            title: "Mission Impossible",
+            poster: "https://vignette.wikia.nocookie.net/missionimpossible/images/c/cf/Mission_Impossible_Fallout_poster.jpg/revision/latest/scale-to-width-down/323?cb=20180202011058"
+          },
+          {
+            title: "Star Wars",
+            poster: "http://www.impawards.com/2017/posters/star_wars_the_last_jedi_ver58.jpg"
+          },
+          {
+            title: "007",
+            poster: "https://66.media.tumblr.com/72eea0fd18af175c7e5ac365156f2220/tumblr_ourocddaad1qc2maxo1_500.png"
+          },
           {
             title: "Dark Knight",
             poster: "https://i.ebayimg.com/images/g/BYwAAOSwmfhX5n5v/s-l300.jpg"
-          },
-          ...this.state.movies
+          }
         ]
       })
-    }, 2000)
+    }, 3000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map( (movie, index) => {
+      console.log(index);
+      // 엘리먼트가 많은 경우 key를 넣어 줘야 함
+      return < Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.movies.map( (movie, index) => {
-          console.log(index);
-          // 엘리먼트가 많은 경우 key를 넣어 줘야 함
-          return < Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
